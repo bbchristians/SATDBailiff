83ebe45145c3390dc6ee305b8de0134f5bb5414e
Ben Christians <bbc7909@rit.edu>
2020-02-11 21:34:04 -0500

    Refactored Commit Locator from interface to abstract class

--- src/main/java/se/rit/edu/git/commitlocator/CommitLocator.java
+++ src/main/java/se/rit/edu/git/commitlocator/CommitLocator.java
@@ -1,11 +1,56 @@
 package se.rit.edu.git.commitlocator;
 
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.comments.Comment;
+import com.github.javaparser.ast.comments.CommentsCollection;
+import org.eclipse.jgit.api.BlameCommand;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.blame.BlameResult;
 import se.rit.edu.satd.SATDInstance;
 
-public interface CommitLocator {
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-    String findCommitIntroduced(Git gitInstance, SATDInstance satdInstance, String v1, String v2);
+public abstract class CommitLocator {
 
-    String findCommitAddressed(Git gitInstance, SATDInstance satdInstance, String v1, String v2);
+    public String findCommitIntroduced(Git gitInstance, SATDInstance satdInstance, String v1, String v2) {
+        try {
+            BlameCommand blameCommand = gitInstance.blame()
+                    .setFilePath(satdInstance.getOldFile())
+                    .setStartCommit(gitInstance.getRepository().resolve(v1));
+            BlameResult blameResult = blameCommand.call();
+            // Parse file as Java
+            JavaParser parser = new JavaParser();
+            Optional<CommentsCollection> comments = parser.parse(
+                    IntStream.range(0, blameResult.getResultContents().size())
+                            .mapToObj(i -> blameResult.getResultContents().getString(i))
+                            .collect(Collectors.joining("\n")))
+                    .getCommentsCollection();
+            if( comments.isPresent() ) {
+                List<Comment> soughtComment = comments.get()
+                        .getComments()
+                        .stream()
+                        .filter(comment -> comment.getContent().trim().equals(satdInstance.getSATDComment().trim()))
+                        .collect(Collectors.toList());
+                if( !soughtComment.isEmpty() &&
+                        soughtComment.get(0).getRange().isPresent() ) {
+                    // TODO Blame all lines of the comment in case two lines blame to different commits
+                    int commentLineNumber = soughtComment.get(0).getRange().get().begin.line;
+                    return blameResult.getSourceCommit(commentLineNumber).getName();
+                }
+                return SATDInstance.COMMIT_UNKNOWN;
+            }
+        } catch (GitAPIException e) {
+            System.err.println("Git API error while blaming file.");
+        } catch (IOException e) {
+            System.err.println("Error resolving commit hash in repository.");
+        }
+        return null;
+    }
+
+    public abstract String findCommitAddressed(Git gitInstance, SATDInstance satdInstance, String v1, String v2);
 }
--- src/main/java/se/rit/edu/git/commitlocator/FileRemovedOrRenamedCommitLocator.java
+++ src/main/java/se/rit/edu/git/commitlocator/FileRemovedOrRenamedCommitLocator.java
@@ -1,12 +1,6 @@
 package se.rit.edu.git.commitlocator;
 
-import com.github.javaparser.JavaParser;
-import com.github.javaparser.ast.comments.Comment;
-import com.github.javaparser.ast.comments.CommentsCollection;
-import org.eclipse.jgit.api.BlameCommand;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.blame.BlameResult;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.diff.RenameDetector;
 import org.eclipse.jgit.errors.CorruptObjectException;
@@ -16,47 +10,8 @@ import se.rit.edu.satd.SATDInstance;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-public class FileRemovedOrRenamedCommitLocator implements CommitLocator {
-
-    @Override
-    public String findCommitIntroduced(Git gitInstance, SATDInstance satdInstance, String v1, String v2) {
-        try {
-            BlameCommand blameCommand = gitInstance.blame()
-                    .setFilePath(satdInstance.getOldFile())
-                    .setStartCommit(gitInstance.getRepository().resolve(v1));
-            BlameResult blameResult = blameCommand.call();
-            // Parse file as Java
-            JavaParser parser = new JavaParser();
-            Optional<CommentsCollection> comments = parser.parse(
-                    IntStream.range(0, blameResult.getResultContents().size())
-                            .mapToObj(i -> blameResult.getResultContents().getString(i))
-                            .collect(Collectors.joining("\n")))
-                .getCommentsCollection();
-            if( comments.isPresent() ) {
-                List<Comment> soughtComment = comments.get()
-                        .getComments()
-                        .stream()
-                        .filter(comment -> comment.getContent().trim().equals(satdInstance.getSATDComment().trim()))
-                        .collect(Collectors.toList());
-                if( !soughtComment.isEmpty() &&
-                        soughtComment.get(0).getRange().isPresent() ) {
-                    // TODO Blame all lines of the comment in case two lines blame to different commits
-                    int commentLineNumber = soughtComment.get(0).getRange().get().begin.line;
-                    return blameResult.getSourceCommit(commentLineNumber).getName();
-                }
-                return SATDInstance.COMMIT_UNKNOWN;
-            }
-        } catch (GitAPIException e) {
-            System.err.println("Git API error while blaming file.");
-        } catch (IOException e) {
-            System.err.println("Error resolving commit hash in repository.");
-        }
-        return null;
-    }
+public class FileRemovedOrRenamedCommitLocator extends CommitLocator {
 
     /**
      *
