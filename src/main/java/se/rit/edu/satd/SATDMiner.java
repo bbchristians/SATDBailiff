e9e6b3508d9d864ba48d6d47b9707dc7fbebe542
Ben Christians <bbc7909@rit.edu>
2020-02-16 14:53:43 -0500

    Cleaned up the miner file and added documentation

--- src/main/java/se/rit/edu/satd/SATDMiner.java
+++ src/main/java/se/rit/edu/satd/SATDMiner.java
@@ -1,6 +1,7 @@
 package se.rit.edu.satd;
 
 import org.apache.commons.io.FileUtils;
+import org.jetbrains.annotations.NotNull;
 import se.rit.edu.git.GitUtil;
 import se.rit.edu.git.RepositoryCommitReference;
 import se.rit.edu.git.RepositoryInitializer;
@@ -12,17 +13,33 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * A class which contains high-level logic for mining SATD Instances from a git repository.
+ */
 public class SATDMiner {
 
+    // Required fields to be set before SATD can be mined
     private String repositoryURI;
-    private RepositoryInitializer repo;
-
     private SATDDetector satdDetector = null;
 
-    public SATDMiner(String repositoryURI) {
+    // A reference to the repository initializes. Stored so it can be cleaned
+    // once mining has completed
+    private RepositoryInitializer repo;
+
+    public SATDMiner(@NotNull String repositoryURI) {
         this.repositoryURI = repositoryURI;
     }
 
+    /**
+     * Generates a list of repository references to be used for other mining actions
+     * @param mostRecentCommit a boundary in which no future tags will be considered
+     *                         -- This is to be used to allow data collection to be replicated
+     *                            across different time periods
+     * @param sortType the way in which commit should be sorted
+     *                 -- The miner will diff release versions as they appear ordered in the list
+     *                    so the ordering of the list will significantly impact the output
+     * @return a list of repository references at all tags in the repository
+     */
     public List<RepositoryCommitReference> getReposAtReleases(String mostRecentCommit, ReleaseSortType sortType) {
         if( repo == null ) {
             this.initializeRepo();
@@ -31,7 +48,6 @@ public class SATDMiner {
         if( sortType == ReleaseSortType.CHRONOLOGICAL ) {
             return refs;
         }
-        // TODO is there a way to sort the refs by the release rather than chronologically?
         return refs;
     }
 
@@ -43,11 +59,19 @@ public class SATDMiner {
         this.satdDetector = detector;
     }
 
+    /**
+     * Iterates over all supplied commits, and outputs a difference in SATD occurrences between
+     * each adjacent commit reference in commitRefs
+     * @param commitRefs a list of supplied commit references to be diffed for SATD
+     * @param writer an OutputWriter that will handle the output of the miner
+     */
     public void writeRepoSATD(List<RepositoryCommitReference> commitRefs, OutputWriter writer) {
+        // Check miner configured correctly
         if( this.satdDetector == null ) {
             System.err.println("Miner does not have SATD Detector set. Please call setSatdDetector() on the Miner object.");
             return;
         }
+        // Go through each commit, and diff against the adjacent commits
         for (int i = 1; i < commitRefs.size(); i++) {
             SATDDifference diff = RepositoryDiffMiner.ofFirstRepository(commitRefs.get(i-1))
                                         .andSecondRepository(commitRefs.get(i))
@@ -62,6 +86,10 @@ public class SATDMiner {
         }
     }
 
+    /**
+     * Cleans the repository that was mined by the Miner. This should delete all files created
+     * by the miner.
+     */
     public void cleanRepo() {
         this.repo.cleanRepo();
         try {
